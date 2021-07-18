51b73a4995167406b77b7bc349df822a578cfee7
DEVIL <84610766+ITZ-DEVIL-OP@users.noreply.github.com>
2021-07-18 19:54:38 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -1,9 +1,8 @@
 from MashaRoBot.events import register
-from MashaRoBot import OWNER_ID
+from MashaRoBot import OWNER_ID, BOT_NAME
 from MashaRoBot import telethn as tbot
 import os 
 from PIL import Image, ImageDraw, ImageFont
-
 
 @register(pattern="^/logo ?(.*)")
 async def lego(event):
@@ -11,62 +10,20 @@ async def lego(event):
  if event.sender_id == OWNER_ID:
      pass
  else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
+ 
+  if not quew:
+     await event.reply('Darling Listen! Provide Some Text To Draw!')
+     return
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./MashaRoBot/resources/Chopsic.otf", 330)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByLovely.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LOVELYR_OBOT")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
-
-
-
-   
-@register(pattern="^/wlogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
+    img = Image.open('./MashaRoBot/resources/logo/logo.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./MashaRoBot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./MashaRoBot/resources/font/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -74,20 +31,56 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogobyTiana.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LOVELYR_OBOT")
+    await tbot.send_file(event.chat_id, fname2, caption="MADE BY @Meiko_Robot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @LOVELYSUPPORTS, {e}')
+   await event.reply(f'Error Report @MissMeikoSupport, {e}')
+
+
+@register(pattern="^/wlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+ 
+  if not quew:
+     await event.reply('Darling Listen! Provide Some Text To Draw!')
+     return
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./MashaRoBot/resources/logo/logo1.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "gold"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./MashaRoBot/resources/font/Chopsic.otf", 330)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="white")
+    fname2 = "LogobyTiana.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="MADE BY @Meiko_Robot")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Report @MissMeikoSupport, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
-
 __help__ = """
- ❍ /logo text :  Create your logo with your name
+ • `/logo text`*:*  Create your logo with your name
+ • `/wlogo`*:*  Create your logo with your name
  """
-__mod_name__ = "ʟᴏɢᴏ ❣️"
+__mod_name__ = "Lᴏɢᴏ❣️"
