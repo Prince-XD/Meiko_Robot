96bf2c893e8815cc21939743e0f07cd39aa6c663
DEVIL <84610766+ITZ-DEVIL-OP@users.noreply.github.com>
2021-07-18 20:00:06 +0530

    Update users.py

--- MashaRoBot/modules/users.py
+++ MashaRoBot/modules/users.py
@@ -1,191 +1,114 @@
-from io import BytesIO
-from time import sleep
+import asyncio
+from asyncio import sleep
 
-from telegram import TelegramError, Update
-from telegram.error import BadRequest, Unauthorized
-from telegram.ext import (
-    CallbackContext,
-    CommandHandler,
-    Filters,
-    MessageHandler,
-    run_async,
+from telethon import events
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
+
+from MashaRoBot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from MashaRoBot.events import register
+
+# =================== CONSTANT ===================
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
 )
 
-import MashaRoBot.modules.sql.users_sql as sql
-from MashaRoBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from MashaRoBot.modules.sql.users_sql import get_all_users
 
-USERS_GROUP = 4
-CHAT_GROUP = 5
-DEV_AND_MORE = DEV_USERS.append(int(OWNER_ID))
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+OFFICERS = [OWNER_ID] + DEV_USERS + DRAGONS + DEMONS
+
+# Check if user has admin rights
+async def is_administrator(user_id: int, message):
+    admin = False
+    async for user in telethn.iter_participants(
+        message.chat_id, filter=ChannelParticipantsAdmins
+    ):
+        if user_id == user.id or user_id in OFFICERS:
+            admin = True
+            break
+    return admin
 
 
-def get_user_id(username):
-    # ensure valid userid
-    if len(username) <= 5:
-        return None
 
-    if username.startswith("@"):
-        username = username[1:]
+@telethn.on(events.NewMessage(pattern=f"^[!/]zombies ?(.*)"))
+async def zombies(event):
+    """ For .zombies command, list all the zombies in a chat. """
 
-    users = sql.get_userid_by_name(username)
+    con = event.pattern_match.group(1).lower()
+    del_u = 0
+    del_status = "No Deleted Accounts Found, Group Is Clean."
 
-    if not users:
-        return None
+    if con != "clean":
+        find_zombies = await event.respond("Searching For Zombies...")
+        async for user in event.client.iter_participants(event.chat_id):
 
-    elif len(users) == 1:
-        return users[0].user_id
+            if user.deleted:
+                del_u += 1
+                await sleep(1)
+        if del_u > 0:
+            del_status = f"Found **{del_u}** Zombies In This Group.\
+            \nClean Them By Using - `/zombies clean`"
+        await find_zombies.edit(del_status)
+        return
 
-    else:
-        for user_obj in users:
+    # Here laying the sanity check
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # Well
+    if not await is_administrator(user_id=event.from_id, message=event):
+        await event.respond("You're Not An Admin!")
+        return
+
+    if not admin and not creator:
+        await event.respond("I Am Not An Admin Here!")
+        return
+
+    cleaning_zombies = await event.respond("Cleaning Zombies...")
+    del_u = 0
+    del_a = 0
+
+    async for user in event.client.iter_participants(event.chat_id):
+        if user.deleted:
             try:
-                userdat = dispatcher.bot.get_chat(user_obj.user_id)
-                if userdat.username == username:
-                    return userdat.id
+                await event.client(
+                    EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS)
+                )
+            except ChatAdminRequiredError:
+                await cleaning_zombies.edit("I Don't Have Ban Rights In This Group.")
+                return
+            except UserAdminInvalidError:
+                del_u -= 1
+                del_a += 1
+            await event.client(EditBannedRequest(event.chat_id, user.id, UNBAN_RIGHTS))
+            del_u += 1
 
-            except BadRequest as excp:
-                if excp.message == "Chat not found":
-                    pass
-                else:
-                    LOGGER.exception("Error extracting user ID")
+    if del_u > 0:
+        del_status = f"Cleaned `{del_u}` Zombies"
 
-    return None
+    if del_a > 0:
+        del_status = f"Cleaned `{del_u}` Zombies \
+        \n`{del_a}` Zombie Admin Accounts Are Not Removed!"
 
-
-@run_async
-@dev_plus
-def broadcast(update: Update, context: CallbackContext):
-    to_send = update.effective_message.text.split(None, 1)
-
-    if len(to_send) >= 2:
-        to_group = False
-        to_user = False
-        if to_send[0] == "/broadcastgroups":
-            to_group = True
-        if to_send[0] == "/broadcastusers":
-            to_user = True
-        else:
-            to_group = to_user = True
-        chats = sql.get_all_chats() or []
-        users = get_all_users()
-        failed = 0
-        failed_user = 0
-        if to_group:
-            for chat in chats:
-                try:
-                    context.bot.sendMessage(
-                        int(chat.chat_id),
-                        to_send[1],
-                        parse_mode="MARKDOWN",
-                        disable_web_page_preview=True,
-                    )
-                    sleep(0.1)
-                except TelegramError:
-                    failed += 1
-        if to_user:
-            for user in users:
-                try:
-                    context.bot.sendMessage(
-                        int(user.user_id),
-                        to_send[1],
-                        parse_mode="MARKDOWN",
-                        disable_web_page_preview=True,
-                    )
-                    sleep(0.1)
-                except TelegramError:
-                    failed_user += 1
-        update.effective_message.reply_text(
-            f"Broadcast complete.\nGroups failed: {failed}.\nUsers failed: {failed_user}."
-        )
-
-
-@run_async
-def log_user(update: Update, context: CallbackContext):
-    chat = update.effective_chat
-    msg = update.effective_message
-
-    sql.update_user(msg.from_user.id, msg.from_user.username, chat.id, chat.title)
-
-    if msg.reply_to_message:
-        sql.update_user(
-            msg.reply_to_message.from_user.id,
-            msg.reply_to_message.from_user.username,
-            chat.id,
-            chat.title,
-        )
-
-    if msg.forward_from:
-        sql.update_user(msg.forward_from.id, msg.forward_from.username)
-
-
-@run_async
-@dev_plus
-def chats(update: Update, context: CallbackContext):
-    all_chats = sql.get_all_chats() or []
-    chatfile = "List of chats.\n0. Chat name | Chat ID | Members count\n"
-    P = 1
-    for chat in all_chats:
-        try:
-            curr_chat = context.bot.getChat(chat.chat_id)
-            bot_member = curr_chat.get_member(context.bot.id)
-            chat_members = curr_chat.get_members_count(context.bot.id)
-            chatfile += "{}. {} | {} | {}\n".format(
-                P, chat.chat_name, chat.chat_id, chat_members
-            )
-            P = P + 1
-        except:
-            pass
-
-    with BytesIO(str.encode(chatfile)) as output:
-        output.name = "groups_list.txt"
-        update.effective_message.reply_document(
-            document=output,
-            filename="groups_list.txt",
-            caption="Here be the list of groups in my database.",
-        )
-
-
-@run_async
-def chat_checker(update: Update, context: CallbackContext):
-    bot = context.bot
-    try:
-        if update.effective_message.chat.get_member(bot.id).can_send_messages is False:
-            bot.leaveChat(update.effective_message.chat.id)
-    except Unauthorized:
-        pass
-
-
-def __user_info__(user_id):
-    if user_id in [777000, 1087968824]:
-        return """╘══「 Groups count: <code>???</code> 」"""
-    if user_id == dispatcher.bot.id:
-        return """╘══「 Groups count: <code>???</code> 」"""
-    num_chats = sql.get_user_num_chats(user_id)
-    return f"""╘══「 Groups count: <code>{num_chats}</code> 」"""
-
-
-def __stats__():
-    return f"• {sql.num_users()} users, across {sql.num_chats()} chats"
-
-
-def __migrate__(old_chat_id, new_chat_id):
-    sql.migrate_chat(old_chat_id, new_chat_id)
-
-
-__help__ = ""  # no help string
-
-BROADCAST_HANDLER = CommandHandler(
-    ["broadcastall", "broadcastusers", "broadcastgroups"], broadcast
-)
-USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
-CHAT_CHECKER_HANDLER = MessageHandler(Filters.all & Filters.group, chat_checker)
-CHATLIST_HANDLER = CommandHandler("groups", chats)
-
-dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
-dispatcher.add_handler(BROADCAST_HANDLER)
-dispatcher.add_handler(CHATLIST_HANDLER)
-dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
-
-__mod_name__ = "Users"
-__handlers__ = [(USER_HANDLER, USERS_GROUP), BROADCAST_HANDLER, CHATLIST_HANDLER]
+    await cleaning_zombies.edit(del_status)
